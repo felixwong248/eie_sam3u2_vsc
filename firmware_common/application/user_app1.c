ddecd4f8db750afe554ce37ae3e6d903d6fd9670
felixwong248 <felixwong248@gmail.com>
2024-09-26 20:22:16 -0600

    test a commit

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -6,6 +6,7 @@ should be replaced by something specific to the task.
 TESTING KEEPING THIS LINE AND DELETING THE ONE BELOW
 
   1. Blah Blah Blah
+  blah
 ----------------------------------------------------------------------------------------------------------------------
 To start a new task using this user_app1 as a template:
  1. Copy both user_app1.c and user_app1.h to the Application directory
